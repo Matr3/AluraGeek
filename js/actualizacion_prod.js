f7c0285e38aea0ee2957f07dc9606981af059f5b
Matr3 <tremari.marce@gmail.com>
2022-08-17 15:24:17 -0300

    arreglos de codigo

--- js/actualizacion_prod.js
+++ js/actualizacion_prod.js
@@ -3,7 +3,7 @@ import { listaServices } from "../service/client-service.js";
 const crearNuevaLinea = (imagen,nombre_prod,precio_prod,id) => {
     const linea = document.createElement("div");
     const contenido = `
-    <div class="articulo" data-producto>
+    <div class="articulo">
     Hola
     <div class="articulo_edicion">
         <a class="position_icon" href="#"><img class="icon" src="./img/editar.png" alt=""></a>
