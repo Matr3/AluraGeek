c45c1532fcd3a9e490f846085dcb677d779aedc3
Matr3 <tremari.marce@gmail.com>
2022-08-17 21:10:47 -0300

    arreglos de codigo

--- js/actualizacion_prod.js
+++ js/actualizacion_prod.js
@@ -9,7 +9,11 @@ const crearNuevaLinea = (imagen,nombre_prod,precio_prod,id) => {
     <div class="articulo_edicion">
         <a class="position_icon" href="editar_productos.html?id=${id}""><img class="icon" src="./img/editar.png" alt=""></a>
         
-        <a class="position_icon del" id="${id}" href=""><img class="icon" src="./img/borrar.png" alt=""></a>
+        <button class="position_icon img_icon" type="button" id="${id}">
+        B
+        </button>
+
+        
     </div>
     <img class="img" src="${imagen}" alt="Imagen del producto">
     </div>
@@ -24,13 +28,13 @@ const crearNuevaLinea = (imagen,nombre_prod,precio_prod,id) => {
     `;
     linea.innerHTML = contenido;
     
-    const btn = linea.querySelector(".del");
+    const btn = linea.querySelector("button");
     btn.addEventListener("click", () => {
       const id = btn.id;
       listaServices
         .eliminarCliente(id)
         .then((respuesta) => {
-          console.log(respuesta);
+          window.location.href = "./productos.html";
         })
         .catch((err) => alert("Ocurrió un error"));
     });
