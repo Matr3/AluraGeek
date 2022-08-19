45b7a087c65cf6a1062f55861f25576871bcb4b7
Ma_tre3 <tremari.marce@gmail.com>
2022-08-19 12:59:36 -0300

    Arreglos js

--- js/detalles_prod.js
+++ js/detalles_prod.js
@@ -2,21 +2,22 @@ import { listaServices } from "../service/client-service.js";
 
 const url = new URL (window.location);
 const id = url.searchParams.get("id");
+
+console.log(id)
 //backticks
-const crearNuevaLinea = (imagen,nombre_prod,precio_prod,descripcion_prod,id) => {
+const crearNuevaLinea = (imagen, nombre_prod, precio_prod, descripcion_prod) => {
     const linea = document.createElement("div");
     
     const contenido = `
-    <section class="producto_principal">
     <div>
-        <img class="img_producto" src="${imagen}" alt="">
+        <img class="img_producto" src="${imagen}" alt="Imagen del Producto ${nombre_prod}">
     </div>
     <div class="detalles">
         <h1 class="nombre_prod">${nombre_prod}</h1>
         <h5 class="precio_prod">${precio_prod}</h5>
         <p class="descripcion_prod">${descripcion_prod}</p>
     </div>
-    </section>`;
+    `;
     linea.innerHTML = contenido;
   
     return linea;
@@ -28,13 +29,14 @@ const div = document.querySelector("[data-detalle]");
 
 
 listaServices
-.detalleProducto()
+.detalleProducto(id)
   .then((data) => {
-    data.forEach(({ imagen,nombre_prod,precio_prod,descripcion_prod,id }) => {
-      const nuevaLinea = crearNuevaLinea(imagen,nombre_prod,precio_prod,descripcion_prod,id);
+    
+      const nuevaLinea = crearNuevaLinea(data.imagen, data.nombre_prod, data.precio_prod, data.descripcion_prod);
       div.appendChild(nuevaLinea);
-    });
+    
   })
   .catch((error) => alert("Oops! Error. Comuniquese con Matr3"));
+
 
 
