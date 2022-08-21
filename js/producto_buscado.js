ed02062e03a3e30dd8f59038a5447c751a917dca
Marcelo Tremari <88912142+Matr3@users.noreply.github.com>
2022-08-21 03:16:08 -0300

    Update producto_buscado.js

--- js/producto_buscado.js
+++ js/producto_buscado.js
@@ -4,8 +4,6 @@ import { listaServices } from "../service/client-service.js";
 const url = new URL(window.location);
 const buscar_prod = url.searchParams.get("buscar");
 
-let cont = 0;
-
 //backticks
 const crearNuevaLinea = (imagen, nombre_prod, precio_prod, descripcion_prod, buscar_prod) => {
     const linea = document.createElement("div");
@@ -31,28 +29,6 @@ const crearNuevaLinea = (imagen, nombre_prod, precio_prod, descripcion_prod, bus
     return linea;
 };
 
-const crearErrorLinea = (buscar_prod) => {
-    const lineaError = document.createElement("div");
-
-    const contenido = `
-        <div class="productos_title">
-            <h2 class="title_full__productos">Buscar los productos ${buscar_prod}</h2>
-        </div>
-        <div class="articulos_descripcion">
-            <div>
-                <img class="img_producto" src="./img/errorBusqueda.png" alt="No se encontro ${buscar_prod}">
-            </div>
-            <div class="detalles">
-                <h1 class="nombre_prod">No se encontro ningun producto con el nombre ${buscar_prod}, realize una nueva busqueda. =)</h1>
-            </div>
-        </div>
-    `;
-
-    lineaError.innerHTML = contenido;
-
-    return lineaError;
-};
-
 const div = document.querySelector("[data-producto]");
 
 listaServices
@@ -62,6 +38,7 @@ listaServices
         console.log(nombre_prod) 
           
         if(buscar_prod == nombre_prod){
+            document.querySelector(".bloqueo").style.display = "none";
             const nuevaLinea = crearNuevaLinea(imagen, nombre_prod, precio_prod, descripcion_prod, buscar_prod);
             div.appendChild(nuevaLinea); 
             }
