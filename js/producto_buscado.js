935d27da3f91ce24b49c21402be2db0037eb8625
Ma_tre3 <tremari.marce@gmail.com>
2022-08-22 02:45:59 -0300

    Arreglos js

--- js/producto_buscado.js
+++ js/producto_buscado.js
@@ -3,7 +3,7 @@ import { listaServices } from "../service/client-service.js";
 
 const url = new URL(window.location);
 const buscar_prod = url.searchParams.get("buscar");
-
+const buscarp = buscar_prod.toLocaleLowerCase();
 //backticks
 const crearNuevaLinea = (imagen, nombre_prod, precio_prod, descripcion_prod, buscar_prod) => {
     const linea = document.createElement("div");
@@ -35,8 +35,8 @@ listaServices
     .then((data) => {
         data.forEach(({ imagen, nombre_prod, precio_prod, descripcion_prod}) => { 
         console.log(nombre_prod) 
-          
-        if(buscar_prod == nombre_prod){
+        const nombre = nombre_prod.toLowerCase()
+        if(buscarp == nombre){
             document.querySelector(".bloqueo_error").style.display = "none";
             document.querySelector(".bloqueo_title").style.display = "block";
             const nuevaLinea = crearNuevaLinea(imagen, nombre_prod, precio_prod, descripcion_prod, buscar_prod);
